1cec48ff95c160455b57aa7a7ab9d9adbdd74569
Avi Romanoff <avi@romanoff.me>
2017-02-23 02:30:34 -0500

    So don't tell Scotty

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ LED_COUNT      = 6      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 200     # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 125     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 
@@ -21,6 +21,9 @@ def setAllColor(strip, color, wait_ms=50):
 
 def blue():
     setAllColor(strip, Color(178, 235, 255))
+
+def green():
+    setAllColor(strip, Color(96, 255, 99))
 
 if __name__ == '__main__':
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
@@ -43,8 +46,8 @@ if __name__ == '__main__':
                 blue()
                 c = sys.stdin.read(1)
                 if c != '\n': continue
-                setAllColor(strip, Color(255, 0, 0))
-                time.sleep(0.25)
+                green()
+                time.sleep(0.75)
             except IOError: pass
     finally:
         termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
